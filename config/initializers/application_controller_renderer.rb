02892474a678ad449bc085f32bfcf92a102ff718
kukki-37 <weiyifu67@gmail.com>
2022-04-13 16:33:50 +0000

    second

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'fe45ce421a7adc6a2296c4520635aaae3ede49072414c6bd7cd120dbbd64423f179e34fdad1885bb9a3ab464f5ff2d31e01dc2763141aa01d79db8747aa4fff5'
